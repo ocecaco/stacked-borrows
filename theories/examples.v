268f33c1602bf8b2a657b942910527637c8af8eb
Hoang-Hai Dang <haidang@mpi-sws.org>
2019-03-13 17:40:49 +0100

    More examples

--- theories/examples.v
+++ theories/examples.v
@@ -36,6 +36,7 @@ Definition demo0 : val :=
   let: "x" := new_pointer_int (ᵗ&mut int) Stack [new_int 1 Heap] in
   (* stack of int: [Raw], not frozen *)
   (* stack of x  : [Uniq(x)], not frozen *)
+  (* x uses Alias(None) for the int *)
 
   (* retag x with new borrow *)
   Retag "x" (ᵗ&mut int) Default ;;
@@ -67,7 +68,7 @@ Definition demo0 : val :=
   (* stack of int: [Uniq(0); Raw], not frozen *)
 
 
-  !(ᵛ*mut{int} (!"y"))
+  !ᵛ*mut{int} (!"y")
   (* Check read  y   with Uniq(y): OK! *)
   (* Check deref int with Uniq(1): UB! Uniq(1) not on stack of int! *)
   (* Check read  int with Uniq(1): unreachable *)
@@ -80,6 +81,7 @@ Definition demo1 : val :=
   let: "x" := new_pointer_int (ᵗ&mut int) Stack [new_int 1 Heap] in
   (* stack of int: [Raw], not frozen *)
   (* stack of x  : [Uniq(x)], not frozen *)
+  (* x uses Alias(None) for the int *)
 
   (* retag x with new borrow *)
   Retag "x" (ᵗ&mut int) Default ;;
@@ -98,7 +100,7 @@ Definition demo1 : val :=
   (* stack of int: [Raw; Uniq(0); Raw], frozen_since(1) *)
   (* y1 now uses Alias(1) for the int *)
 
-  !(ᵛ*mut{int} (!"x")) ;;
+  !ᵛ*mut{int} (!"x") ;;
   (* Check read  x   with Uniq(x): OK! *)
   (* Check deref int with Uniq(0): OK! because Uniq(0) is on the stack *)
   (* Check read  int with Uniq(0): OK! because the stack is frozen *)
@@ -116,12 +118,12 @@ Definition demo1 : val :=
   (* stack of int: [Raw; Uniq(0); Raw], frozen_since(1) *)
   (* y2 now uses Alias(2) for the int *)
 
-  !(ᵛ*imm{int} (!"y1")) ;;
+  !ᵛ*imm{int} (!"y1") ;;
   (* Check read  y1  with Uniq(y1): OK! *)
   (* Check deref y1  with Alias(1): OK! because the stack is frozen_since(1) *)
   (* Check read  int with Alias(1): OK! because the stack is frozen *)
 
-  !(ᵛ*imm{int} (!"y2"))
+  !ᵛ*imm{int} (!"y2")
   (* Check read  y2  with Uniq(y2): OK! *)
   (* Check deref y2  with Alias(2): OK! because the stack is frozen_since(1) *)
   (* Check read  int with Alias(2): OK! because the stack is frozen *)
@@ -134,6 +136,7 @@ Definition demo2 : val :=
   let: "x" := new_pointer_int (ᵗ&mut int) Stack [new_int 1 Heap] in
   (* stack of int: [Raw], not frozen *)
   (* stack of x  : [Uniq(x)], not frozen *)
+  (* x uses Alias(None) for the int *)
 
   (* retag x with new borrow *)
   Retag "x" (ᵗ&mut int) Default ;;
@@ -157,7 +160,7 @@ Definition demo2 : val :=
   (* let z = x as *const u8 as *mut u8; *)
   let: "z" := new_pointer_int (ᵗ* int) Stack [ ᵛ& ᵛ*raw{int} ᵛ& ᵛ*imm{int} (!"x") ] in
   (* Check read  x   with Uniq(x): OK! *)
-  (* Check deref int with Uniq(0): OK! because Uniq(0) is in the stack and
+  (* Check deref int with Uniq(0): OK! because Uniq(0) is on the stack and
                                        raw derefs are not checked. *)
   (* stack of z  : [Uniq(z)], not frozen *)
 
@@ -166,16 +169,79 @@ Definition demo2 : val :=
   (* stack of int: [Raw; Uniq(0); Raw], frozen_since(1) *)
   (* z now uses Alias(None) for the int *)
 
-  (ᵛ*raw{int} (!"z")) <- #3 ;;
+  ᵛ*raw{int} (!"z") <- #3 ;;
   (* Check read  z   with Uniq(z)    : OK! *)
   (* Check deref int with Alias(None): OK! because raw derefs are not checked. *)
   (* Check write int with Alias(None): OK! because there matches a Raw.
                                            Furthermore, the stack is unfrozen. *)
   (* stack of int: [Raw; Uniq(0); Raw], not frozen *)
 
-  !(ᵛ*imm{int} (!"y"))
+  !ᵛ*imm{int} (!"y")
   (* Check read  y   with Uniq(y) : OK! *)
   (* Check deref int with Alias(1): UB! because the stack is not frozen with a
-                                    timestamp less than or equal to the tag. *)
+                                    timestamp less than or equal to the tag.
+                                    The type int doesn't contain UnsafeCell. *)
   (* Check read  int with Alias(1): unreachable *)
+  .
+
+(* UB *)
+Definition demo4 : val :=
+  λ: [<>],
+  (* let x = &mut 1u8; *)
+  let: "x" := new_pointer_int (ᵗ&mut int) Stack [new_int 1 Heap] in
+  (* stack of int: [Raw], not frozen *)
+  (* stack of x  : [Uniq(x)], not frozen *)
+  (* x uses Alias(None) for the int *)
+
+  (* retag x with new borrow *)
+  Retag "x" (ᵗ&mut int) Default ;;
+  (* stack of int: [Uniq(0); Raw], not frozen *)
+  (* x now uses Uniq(0) for the int *)
+
+  (* raw pointer y1 created from x *)
+  (* let y1 = x as *mut u8; *)
+  let: "y1" := new_pointer_int (ᵗ* int) Stack [ᵛ& ᵛ*mut{int} (!"x")] in
+  (* Check read  x   with Uniq(x): OK! *)
+  (* Check deref int with Uniq(0): OK! *)
+  (* stack of y1 : [Uniq(y1)], not frozen *)
+
+  (* retag y with new borrow *)
+  Retag "y1" (ᵗ* int) RawRt ;;
+  (* stack of int: [Raw; Uniq(0); Raw], not frozen *)
+  (* y1 now uses Alias(None) for the int *)
+
+  (* raw pointer y2 created from y1. *)
+  (* let y2 = y1; *)
+  let: "y2" := new_pointer_int (ᵗ* int) Stack [ ᵛ& ᵛ*raw{int} (!"y1") ] in
+  (* Check read  y1  with Uniq(y1)   : OK! *)
+  (* Check deref int with Alias(None): OK! because Raw is on the stack. *)
+  (* stack of y2  : [Uniq(y2)], not frozen *)
+
+  (* retag y2 with new borrow *)
+  Retag "y2" (ᵗ* int) RawRt ;;
+  (* stack of int: [Raw; Uniq(0); Raw], not frozen *)
+  (* y2 now uses Alias(None) for the int *)
+
+  ᵛ*raw{int} (!"y1") <- #3 ;;
+  ᵛ*raw{int} (!"y2") <- #5 ;;
+  ᵛ*raw{int} (!"y2") <- ! ᵛ*raw{int} (!"y1") ;;
+  (* Checks for deref/read/write of y1 and y2 all pass because:
+     * raw derefs are not checked
+     * read/write are ok because Raw is on top of the stack, and nothing changes. *)
+  (* stack of int: [Raw; Uniq(0); Raw], not frozen *)
+
+  ᵛ*mut{int} (!"x") <- #7 ;;
+  (* Check read  x   with Uniq(x): OK! *)
+  (* Check deref int with Uniq(0): OK! because Uniq(0) is on the stack *)
+  (* Check write int with Uniq(0): OK! because Uniq(0) is on the stack *)
+  (* stack of int: [Uniq(0); Raw], not frozen *)
+
+  !ᵛ*raw{int} (!"y1")
+  (* Check read  y1  with Uniq(y1)   : OK! *)
+  (* Check deref int with Alias(None): OK! because Raw is on the stack *)
+  (* Check read  int with Alias(None): OK! because Raw is on the stack *)
+  (* stack of int: [Raw], not frozen *)
+  (* TODO: so why is this UB?!? The example in the blog post assumes the stack
+     to be [Uniq(0)], but here we are with [Uniq(0); Raw], due to the
+     initialization of the stack for heap locations *)
   .
