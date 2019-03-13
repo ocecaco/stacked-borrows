70f8a69aeb8ad1640ce138bee4d5a121037e1569
Hoang-Hai Dang <haidang@mpi-sws.org>
2019-03-13 15:03:08 +0100

    Add some examples for retagging

--- theories/examples.v
+++ theories/examples.v
@@ -3,26 +3,179 @@ From stbor Require Import notation tactics.
 (** Some common types *)
 Notation int := (Scala 1).
 Notation int_arr n := (Scala n).
-Notation "'ᵗ&mut' T" := (Reference (RefPtr Mutable) T) (at level 10, format "ᵗ&mut  T").
-Notation "'ᵗ&' T" := (Reference (RefPtr Immutable) T) (at level 10, format "ᵗ&  T").
+Notation "'ᵗ&mut' T" := (Reference (RefPtr Mutable) T)
+  (at level 10, format "ᵗ&mut  T").
+Notation "'ᵗ&' T" := (Reference (RefPtr Immutable) T)
+  (at level 10, format "ᵗ&  T").
 Notation "'ᵗ*' T" := (Reference RawPtr T) (at level 10, format "ᵗ*  T").
 Notation "'Box<' T '>'" := (Reference RawPtr T) (at level 10, format "Box< T >").
 
 
 Notation "ᵛ& e" := (Ref e%E) (at level 9, format "ᵛ& e") : expr_scope.
-Notation "ᵛ*{ T , N } e" := (Deref e%E T N) (at level 8, format "ᵛ*{ T ,  N }  e") : expr_scope.
+Notation "ᵛ*mut{ T } e" := (Deref e%E T (Some Mutable))
+  (at level 8, format "ᵛ*mut{ T }  e") : expr_scope.
+Notation "ᵛ*imm{ T } e" := (Deref e%E T (Some Immutable))
+  (at level 8, format "ᵛ*imm{ T }  e") : expr_scope.
+Notation "ᵛ*raw{ T } e" := (Deref e%E T None)
+  (at level 8, format "ᵛ*raw{ T }  e") : expr_scope.
 
-Definition new_zero amod : val :=
-  λ: [<>], let: "i" := Alloc int amod in "i" <- #0 ;; ᵛ&"i".
-Definition new_mut_ref_int amod : val :=
+(* These two are supposed to be inlined, so they don't contribute to creating
+  call ids or retagging. *)
+Definition new_int (i: Z) amod : val :=
+  λ: [<>], let: "i" := Alloc int amod in "i" <- #i ;; ᵛ&"i".
+Definition new_pointer_int T amod : val :=
   λ: ["v"],
-    let: "x" := Alloc (ᵗ&mut int) amod in
+    let: "x" := Alloc T amod in
       "x" <- "v" ;; "x".
 
+(* UB *)
+(* from https://www.ralfj.de/blog/2018/11/16/stacked-borrows-implementation.html *)
 Definition demo0 : val :=
-  λ: ["x"],
-  let: "y" := new_mut_ref_int Stack [ ᵛ& ᵛ*{int, Some Mutable} (!"x") ] in
+  λ: [<>],
+  (* let x = &mut 1u8; *)
+  let: "x" := new_pointer_int (ᵗ&mut int) Stack [new_int 1 Heap] in
+  (* stack of int: [Raw], not frozen *)
+  (* stack of x  : [Uniq(x)], not frozen *)
+
+  (* retag x with new borrow *)
+  Retag "x" (ᵗ&mut int) Default ;;
+  (* stack of int: [Uniq(0); Raw], not frozen *)
+  (* x now uses Uniq(0) for the int *)
+
+  (* mut reference y created by dereferencing mut reference x *)
+  (* let y = &mut *x; *)
+  let: "y" := new_pointer_int (ᵗ&mut int) Stack [ ᵛ& ᵛ*mut{int} (!"x") ] in
+  (* Check read  x   with Uniq(x): OK! *)
+  (* Check deref int with Uniq(0): OK! *)
+  (* stack of y  : [Uniq(y)], not frozen *)
+
+  (* retag y with new borrow *)
   Retag "y" (ᵗ&mut int) Default ;;
-  ᵛ*{int, Some Mutable} (!"y") <- #5 ;;
-  ᵛ*{int, Some Mutable} (!"x") <- #3 ;;
-  !(ᵛ*{int, Some Mutable} (!"y")).
+  (* stack of int: [Uniq(1); Uniq(0); Raw], not frozen *)
+  (* y now uses Uniq(1) for the int *)
+
+  ᵛ*mut{int} (!"y") <- #5 ;;
+  (* Check read  y   with Uniq(y): OK! *)
+  (* Check deref int with Uniq(1): OK! because Uniq(1) in on the stack *)
+  (* Check write int with Uniq(1): OK! *)
+  (* stack of int: [Uniq(1); Uniq(0); Raw], not frozen *)
+
+  ᵛ*mut{int} (!"x") <- #3 ;;
+  (* Check read  x   with Uniq(x): OK! *)
+  (* Check deref int with Uniq(0): OK! because Uniq(0) in on the stack *)
+  (* Check write int with Uniq(0): OK! but pop Uniq(1) to reactivate Uniq(0) *)
+  (* stack of int: [Uniq(0); Raw], not frozen *)
+
+
+  !(ᵛ*mut{int} (!"y"))
+  (* Check read  y   with Uniq(y): OK! *)
+  (* Check deref int with Uniq(1): UB! Uniq(1) not on stack of int! *)
+  (* Check read  int with Uniq(1): unreachable *)
+  .
+
+(* DB *)
+Definition demo1 : val :=
+  λ: [<>],
+  (* let x = &mut 1u8; *)
+  let: "x" := new_pointer_int (ᵗ&mut int) Stack [new_int 1 Heap] in
+  (* stack of int: [Raw], not frozen *)
+  (* stack of x  : [Uniq(x)], not frozen *)
+
+  (* retag x with new borrow *)
+  Retag "x" (ᵗ&mut int) Default ;;
+  (* stack of int: [Uniq(0); Raw], not frozen *)
+  (* x now uses Uniq(0) for the int *)
+
+  (* immut reference y1 created by dereferencing mut reference x *)
+  (* let y1 = & *x; *)
+  let: "y1" := new_pointer_int (ᵗ& int) Stack [ ᵛ& ᵛ*mut{int} (!"x") ] in
+  (* Check read  x   with Uniq(x): OK! *)
+  (* Check deref int with Uniq(0): OK! *)
+  (* stack of y1  : [Uniq(y1)], not frozen *)
+
+  (* retag y1 with new borrow *)
+  Retag "y1" (ᵗ& int) Default ;;
+  (* stack of int: [Raw; Uniq(0); Raw], frozen_since(1) *)
+  (* y1 now uses Alias(1) for the int *)
+
+  !(ᵛ*mut{int} (!"x")) ;;
+  (* Check read  x   with Uniq(x): OK! *)
+  (* Check deref int with Uniq(0): OK! because Uniq(0) is on the stack *)
+  (* Check read  int with Uniq(0): OK! because the stack is frozen *)
+  (* stack of int: [Raw; Uniq(0); Raw], frozen_since(1) *)
+
+  (* immut reference y2 created by dereferencing mut reference x *)
+  (* let y2 = & *x; *)
+  let: "y2" := new_pointer_int (ᵗ& int) Stack [ ᵛ& ᵛ*mut{int} (!"x") ] in
+  (* Check read  x   with Uniq(x): OK! *)
+  (* Check deref int with Uniq(0): OK! *)
+  (* stack of y2  : [Uniq(y2)], not frozen *)
+
+  (* retag y2 with new borrow *)
+  Retag "y2" (ᵗ& int) Default ;;
+  (* stack of int: [Raw; Uniq(0); Raw], frozen_since(1) *)
+  (* y2 now uses Alias(2) for the int *)
+
+  !(ᵛ*imm{int} (!"y1")) ;;
+  (* Check read  y1  with Uniq(y1): OK! *)
+  (* Check deref y1  with Alias(1): OK! because the stack is frozen_since(1) *)
+  (* Check read  int with Alias(1): OK! because the stack is frozen *)
+
+  !(ᵛ*imm{int} (!"y2"))
+  (* Check read  y2  with Uniq(y2): OK! *)
+  (* Check deref y2  with Alias(2): OK! because the stack is frozen_since(1) *)
+  (* Check read  int with Alias(2): OK! because the stack is frozen *)
+  .
+
+(* UB *)
+Definition demo2 : val :=
+  λ: [<>],
+  (* let x = &mut 1u8; *)
+  let: "x" := new_pointer_int (ᵗ&mut int) Stack [new_int 1 Heap] in
+  (* stack of int: [Raw], not frozen *)
+  (* stack of x  : [Uniq(x)], not frozen *)
+
+  (* retag x with new borrow *)
+  Retag "x" (ᵗ&mut int) Default ;;
+  (* stack of int: [Uniq(0); Raw], not frozen *)
+  (* x now uses Uniq(0) for the int *)
+
+  (* immut reference y created by dereferencing mut reference x *)
+  (* let y = & *x; *)
+  let: "y" := new_pointer_int (ᵗ& int) Stack [ ᵛ& ᵛ*mut{int} (!"x") ] in
+  (* Check read  x   with Uniq(x): OK! *)
+  (* Check deref int with Uniq(0): OK! *)
+  (* stack of y  : [Uniq(y)], not frozen *)
+
+  (* retag y with new borrow *)
+  Retag "y" (ᵗ& int) Default ;;
+  (* stack of int: [Raw; Uniq(0); Raw], frozen_since(1) *)
+  (* y now uses Alias(1) for the int *)
+
+  (* raw pointer z created by dereferencing x as raw pointer immutably then
+     mutably. *)
+  (* let z = x as *const u8 as *mut u8; *)
+  let: "z" := new_pointer_int (ᵗ* int) Stack [ ᵛ& ᵛ*raw{int} ᵛ& ᵛ*imm{int} (!"x") ] in
+  (* Check read  x   with Uniq(x): OK! *)
+  (* Check deref int with Uniq(0): OK! because Uniq(0) is in the stack and
+                                       raw derefs are not checked. *)
+  (* stack of z  : [Uniq(z)], not frozen *)
+
+  (* retag z with new borrow *)
+  Retag "z" (ᵗ* int) RawRt ;;
+  (* stack of int: [Raw; Uniq(0); Raw], frozen_since(1) *)
+  (* z now uses Alias(None) for the int *)
+
+  (ᵛ*raw{int} (!"z")) <- #3 ;;
+  (* Check read  z   with Uniq(z)    : OK! *)
+  (* Check deref int with Alias(None): OK! because raw derefs are not checked. *)
+  (* Check write int with Alias(None): OK! because there matches a Raw.
+                                           Furthermore, the stack is unfrozen. *)
+  (* stack of int: [Raw; Uniq(0); Raw], not frozen *)
+
+  !(ᵛ*imm{int} (!"y"))
+  (* Check read  y   with Uniq(y) : OK! *)
+  (* Check deref int with Alias(1): UB! because the stack is not frozen with a
+                                    timestamp less than or equal to 1. *)
+  (* Check read  int with Alias(1): unreachable *)
+  .
