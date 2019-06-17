204636fa9efb1357f76270d60f98e84a0df53d1d
Hoang-Hai Dang <haidang@mpi-sws.org>
2019-06-17 13:43:49 +0200

    fix local simulation

--- theories/simulation_local.v
+++ theories/simulation_local.v
@@ -8,107 +8,136 @@ Context {A: ucmraT}.
 Variable (wsat: A → state → state → Prop).
 Variable (vrel: A → expr → expr → Prop).
 Variable (fns_src fns_tgt: fn_env).
-Variable (Φinit: A → state → state → Prop).
-
-Section local_step.
-(* Variable (c_src: call_id) (cids_entry_src: call_id_stack)
-         (c_tgt: call_id) (cids_entry_tgt: call_id_stack). *)
 
 (* TODO: encode what it means for the context to not contain our private tags! *)
 (* TODO: how do we know we're at the top of the stack? *)
 Inductive _sim_fn_body
-  (sim_fn_body : A → A → expr → state → expr → state → Prop)
-  (r_f r: A) : expr → state → expr → state → Prop :=
+  (sim_fn_body : A → expr → state → expr → state → Prop)
+  (r: A) : expr → state → expr → state → Prop :=
 (* If src is stuck, then anything is related *)
 | sim_fn_body_stuck e_src σ_src e_tgt σ_tgt
     (STUCK: stuck e_src (mkConfig fns_src σ_src))
-    : _sim_fn_body sim_fn_body r_f r e_src σ_src e_tgt σ_tgt
+    : _sim_fn_body sim_fn_body r e_src σ_src e_tgt σ_tgt
 (* If tgt makes 1 step, src can make some step *)
 | sim_fn_body_step e_src σ_src e_tgt σ_tgt
-    (STEP: ∀ e_tgt' cfg_tgt',
+    (STEP: ∀ r_f e_tgt' cfg_tgt',
+      (* for any frame r_f that is compatible with our resource r and has world satisfaction *)
+      ✓ (r_f ⋅ r) →
+      wsat (r_f ⋅ r) σ_src σ_tgt →
       (* if tgt can locally make 1 step *)
       (e_tgt, (mkConfig fns_tgt σ_tgt)) ~t~> (e_tgt', cfg_tgt') →
-      (* then we locally can makes some step *)
-      ∃ e_src' cfg_src',
+      (* then we locally can makes some step and pick a new resource r' *)
+      ∃ e_src' cfg_src' r',
         (e_src, (mkConfig fns_src σ_src)) ~t~>* (e_src', cfg_src') ∧
-        sim_fn_body r_f r e_src' cfg_src'.(cst) e_tgt' cfg_tgt'.(cst))
-    : _sim_fn_body sim_fn_body r_f r e_src σ_src e_tgt σ_tgt
+        ✓ (r_f ⋅ r') ∧ wsat (r_f ⋅ r') cfg_src'.(cst) cfg_tgt'.(cst) ∧
+        sim_fn_body r' e_src' cfg_src'.(cst) e_tgt' cfg_tgt'.(cst))
+    : _sim_fn_body sim_fn_body r e_src σ_src e_tgt σ_tgt
 (* If tgt prepares to make a call to [name], src should be able to make the same
-  call. Here we do not want to step into the call of [name], but to step over
-  it. To make such a call, we need to re-establish
- `wsat (r ⋅ r_f) cfg_src.(cst) σ_tgt`. Then after the call we can assume that:
-  - the context K_src/tgt did not change
-  - the call id stack did not change
-  - the framed resources r ⋅ r_f did not change
-  - wsat is re-established by [name] for some new frame and new states. *)
-| sim_fn_body_step_over_call K_src K_tgt name e_src el_src el_tgt σ_src rc rv
-    cfg_src σ_tgt
-    (STEPS: (e_src, (mkConfig fns_src σ_src)) ~t~>*
-            (fill K_src (Call #(LitFnPtr name) el_src), cfg_src))
-    (VALS: Forall (λ ei, terminal ei) el_src)
-    (VALT: Forall (λ ei, terminal ei) el_tgt)
-    (VALEQ: Forall2 (vrel (rv)) el_src el_tgt)
-    (VALID: ✓ (r_f ⋅ rc ⋅ rv))
-    (INV: wsat (r_f ⋅ rc ⋅ rv) cfg_src.(cst) σ_tgt)
-    (RET: ∀ v_src v_tgt r' cfg_src' cfg_tgt',
-      (* For any new resource r' that is compatible with our local resource r and frame r_f *)
-      ✓ (r_f ⋅ rc ⋅ r') →
-      (* and wsat holds for the composed resource + pair of physical states, *)
-      wsat (r_f ⋅ rc ⋅ r') cfg_src'.(cst) cfg_tgt'.(cst) →
-      (* and the returned values are related w.r.t. (r ⋅ r' ⋅ r_f) *)
-      vrel (r_f ⋅ rc ⋅ r') (of_val v_src) (of_val v_tgt) →
-      (* (* stack unchanged *)
-      cfg_src'.(cst).(scs) = cfg_src.(cst).(scs) →
-      cfg_tgt'.(cst).(scs) = σ_tgt.(scs) → *)
-      sim_fn_body r_f (rc ⋅ r')
-                (fill K_src (of_val v_src)) cfg_src'.(cst)
-                (fill K_tgt (of_val v_tgt)) cfg_tgt'.(cst))
-    : _sim_fn_body sim_fn_body r_f r e_src σ_src
-        (fill K_tgt (Call #(LitFnPtr name) el_tgt)) σ_tgt
+  call. Here we do not want to step into the call of [name], but to step over it. *)
+| sim_fn_body_step_over_call K_src K_tgt name e_src el_tgt σ_src σ_tgt
+    (STEPOVER: ∀ r_f,
+        (* for any frame r_f that is compatible with our resource r and has world satisfaction *)
+        ✓ (r_f ⋅ r) → wsat (r_f ⋅ r) σ_src σ_tgt →
+        (* tgt is ready to make a call of [name] *)
+        Forall (λ ei, is_Some (to_value ei)) el_tgt →
+        (* then src is also ready to make a call of [name] *)
+        ∃ el_src cfg_src rc rv,
+        (e_src, (mkConfig fns_src σ_src)) ~t~>*
+          (fill K_src (Call #[ScFnPtr name] el_src), cfg_src) ∧
+        Forall (λ ei, is_Some (to_value ei)) el_src ∧
+        (* and we can pick a resource [rv] for the arguments *)
+        ✓ (r_f ⋅ (rc ⋅ rv)) ∧ wsat (r_f ⋅ (rc ⋅ rv)) cfg_src.(cst) σ_tgt ∧
+        (* [rv] justifies the arguments *)
+        Forall2 (vrel rv) el_src el_tgt ∧
+        (* and after the call our context can continue *)
+        (∀ r_f' r' v_src v_tgt cfg_src' cfg_tgt',
+          (* For any new resource r' and frame r_f' that are compatible with
+             our local resource r and has world satisfaction *)
+          ✓ (r_f' ⋅ (rc ⋅ r')) →
+          wsat (r_f ⋅ (rc ⋅ r')) cfg_src'.(cst) cfg_tgt'.(cst) →
+          (* and the returned values are related w.r.t. (r ⋅ r' ⋅ r_f) *)
+          vrel r' (Val v_src) (Val v_tgt) →
+          (* (* stack unchanged *)
+          cfg_src'.(cst).(scs) = cfg_src.(cst).(scs) →
+          cfg_tgt'.(cst).(scs) = σ_tgt.(scs) → *)
+          sim_fn_body (rc ⋅ r')
+                      (fill K_src (Val v_src)) cfg_src'.(cst)
+                      (fill K_tgt (Val v_tgt)) cfg_tgt'.(cst)))
+    : _sim_fn_body sim_fn_body r e_src σ_src
+        (fill K_tgt (Call #[ScFnPtr name] el_tgt)) σ_tgt
 (* If tgt prepares to end, src also prepares to end *)
 | sim_fn_body_end_call v_src σ_src v_tgt σ_tgt
-    (GUAR: ∀ cfg_tgt',
-        (* ✓ (r ⋅ r_f) → *)
+    (GUAR: ∀ r_f cfg_tgt',
+        ✓ (r_f ⋅ r) → wsat (r_f ⋅ r) σ_src σ_tgt →
         (* if tgt can end call *)
-        (EndCall (of_val v_tgt), (mkConfig fns_tgt σ_tgt)) ~t~> cfg_tgt' →
+        (EndCall (Val v_tgt), (mkConfig fns_tgt σ_tgt)) ~t~> cfg_tgt' →
         (* then src can also end call *)
         ∃ cfg_src' r',
-        (EndCall (of_val v_src), (mkConfig fns_src σ_tgt)) ~t~> cfg_src' ∧
+        (EndCall (Val v_src), (mkConfig fns_src σ_tgt)) ~t~> cfg_src' ∧
         (* and re-establish wsat *)
         ✓ (r_f ⋅ r') ∧ wsat (r_f ⋅ r') cfg_src'.2.(cst) cfg_tgt'.2.(cst) ∧
         (* and the returned values are related *)
-        vrel (r_f ⋅ r') (of_val v_src) (of_val v_tgt)
+        vrel r' (Val v_src) (Val v_tgt)
         (* (* and call id stacks are restored *)
         cfg_src'.2.(cst).(scs) = cids_entry_src ∧
         cfg_tgt'.2.(cst).(scs) = cids_entry_tgt  *))
-    : _sim_fn_body sim_fn_body r_f r (EndCall (of_val v_src)) σ_src (EndCall (of_val v_tgt)) σ_tgt
+    : _sim_fn_body sim_fn_body r (EndCall (Val v_src)) σ_src (EndCall (Val v_tgt)) σ_tgt
 .
 
-Lemma sim_fn_body_mono : monotone6 _sim_fn_body.
+Lemma sim_fn_body_mono : monotone5 _sim_fn_body.
 Proof.
-  intros r_f r es σs et σt R R' SIM LE; inversion SIM; subst.
+  intros r es σs et σt R R' SIM LE; inversion SIM; subst.
   - by eapply sim_fn_body_stuck; eauto.
-  - eapply sim_fn_body_step. intros et' cfgt' STEPT.
-    destruct (STEP _ _ STEPT) as (es' & cfgs' & STEPS & Hr).
-    naive_solver.
-  - by eapply sim_fn_body_step_over_call; eauto.
+  - eapply sim_fn_body_step. naive_solver.
+  - eapply sim_fn_body_step_over_call; eauto. naive_solver.
   - by eapply sim_fn_body_end_call; eauto.
 Qed.
 
-Definition sim_fn_body := paco6 _sim_fn_body bot6.
+Definition sim_fn_body := paco5 _sim_fn_body bot5.
 
-End local_step.
-
-Definition sim_fn_body_expr (r: A) (e_src e_tgt: expr) : Prop :=
-  ∀ (r_f: A) (σ_src σ_tgt: state)
-    (VAL: ✓ (r_f ⋅ r)) (INV: wsat (r_f ⋅ r) σ_src σ_tgt),
-  sim_fn_body r_f r e_src σ_src e_tgt σ_tgt.
+Lemma sim_fn_body_resource_mono r1 r2
+  (SAT_DOWNWARD: ∀ r1 r2, r1 ≼ r2 → wsat r2 <2= wsat r1) :
+  r1 ≼ r2 → sim_fn_body r1 <4= sim_fn_body r2.
+Proof.
+  revert r1 r2. pcofix CIH. intros r1 r2 LE es σs et σt SIM.
+  pfold. punfold SIM; [|apply sim_fn_body_mono].
+  inversion SIM; subst.
+  - by eapply sim_fn_body_stuck.
+  - eapply sim_fn_body_step.
+    intros r_f et' ct' VL2 WS2 STEPT.
+    have VL1: ✓ (r_f ⋅ r1).
+    { eapply cmra_valid_included; [exact VL2|by apply cmra_mono_l]. }
+    have WS1: wsat (r_f ⋅ r1) σs σt.
+    { eapply SAT_DOWNWARD; [by apply cmra_mono_l|exact WS2]. }
+    destruct (STEP _ _ _ VL1 WS1 STEPT) as (es' & cs' & r' & STEPS' & VL' & WS' & SIM').
+    exists es', cs', r'. do 3 (split; [done|]).
+    right. eapply CIH; eauto. by inversion SIM'.
+  - eapply sim_fn_body_step_over_call; eauto.
+    intros r_f VL2 WS2 FAT.
+    have VL1: ✓ (r_f ⋅ r1).
+    { eapply cmra_valid_included; [exact VL2|by apply cmra_mono_l]. }
+    have WS1: wsat (r_f ⋅ r1) σs σt.
+    { eapply SAT_DOWNWARD; [by apply cmra_mono_l|exact WS2]. }
+    destruct (STEPOVER _ VL1 WS1 FAT)
+      as (es' & cs & rc & rv & STEPS & FAS & VL & WS & VREL & HK).
+    exists es', cs, rc, rv. do 5 (split; [done|]).
+    intros r_f' r' vs vt cs' ct' VL' WS' VREL'.
+    specialize (HK _ _ _ _ _ _ VL' WS' VREL').
+    right. eapply CIH; [eauto|by inversion HK].
+  - eapply sim_fn_body_end_call.
+    intros r_f ct' VL2 WS2 STEPT.
+    have VL1: ✓ (r_f ⋅ r1).
+    { eapply cmra_valid_included; [exact VL2|by apply cmra_mono_l]. }
+    have WS1: wsat (r_f ⋅ r1) σs σt.
+    { eapply SAT_DOWNWARD; [by apply cmra_mono_l|exact WS2]. }
+    naive_solver.
+Qed.
 
 (* Simulating functions: assuming the calls have been initialized. *)
-Definition sim_fn_body_fn (fn_src fn_tgt : function) : Prop :=
-  ∀ r e_src e_tgt el_src el_tgt
-    (VALS: Forall (λ ei, terminal ei) el_src)
-    (VALT: Forall (λ ei, terminal ei) el_tgt)
+Definition sim_local_fn (fn_src fn_tgt : function) : Prop :=
+  ∀ r e_src e_tgt el_src el_tgt σ_src σ_tgt
+    (VALS: Forall (λ ei, is_Some (to_value ei)) el_src)
+    (VALT: Forall (λ ei, is_Some (to_value ei)) el_tgt)
     (VALEQ: Forall2 (vrel r) el_src el_tgt)
     (EQS: match fn_src with
           | FunV xl e => subst_l xl el_src e = Some e_src
@@ -116,32 +145,11 @@ Definition sim_fn_body_fn (fn_src fn_tgt : function) : Prop :=
     (EQT: match fn_tgt with
           | FunV xl e => subst_l xl el_tgt e = Some e_tgt
           end),
-    sim_fn_body_expr r (InitCall e_src) (InitCall e_tgt).
+    sim_fn_body r (InitCall e_src) σ_src (InitCall e_tgt) σ_tgt.
 
-Definition sim_fn_body_fns : Prop :=
-  ∀ name fn_src, fns_src !! name = Some fn_src → ∃ r fn_tgt,
-    fns_tgt !! name = Some fn_tgt ∧ sim_fn_body_fn r fn_src fn_tgt.
-
-Lemma sim_fn_body_resource_mono r_f r1 r2 :
-  r1 ≼ r2 → sim_fn_body r_f r1 <4= sim_fn_body r_f r2.
-Proof.
-  revert r1 r2. pcofix CIH. intros r1 r2 LE es σs et σt SIM.
-  pfold. punfold SIM; [|apply sim_fn_body_mono].
-  inversion SIM; subst.
-  - by eapply sim_fn_body_stuck.
-  - eapply sim_fn_body_step.
-    intros et' ct' STEP'.
-    destruct (STEP _ _ STEP') as (es' & cs' & STEPS' & SIM').
-    exists es', cs'. split; [done|].
-    right. eapply CIH; eauto. by inversion SIM'.
-  - eapply sim_fn_body_step_over_call; eauto.
-    intros vs vt r' cs' ct' VAL WSAT VREL.
-    specialize (RET _ _ _ _ _ VAL WSAT VREL).
-    right. eapply CIH; [eauto|by inversion RET].
-  - eapply sim_fn_body_end_call.
-    intros ct' STEP'. destruct (GUAR _ STEP') as (es' & r' & STEPS' & VAL & WSAT & VREL).
-    naive_solver.
-Qed.
+Definition sim_local_fns : Prop :=
+  ∀ name fn_src, fns_src !! name = Some fn_src → ∃ fn_tgt,
+    fns_tgt !! name = Some fn_tgt ∧ sim_local_fn fn_src fn_tgt.
 
 End local.
 
